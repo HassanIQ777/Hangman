bb239e3ee5b713a7f4f74f644df5d419167d2ac1
HassanIQ777 <hassaniq793@gmail.com>
2026-06-29 00:03:48 +0300

    Removed using namespace std from main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,19 @@
 #include <cctype>
 #include <ctime>
 
-using namespace std;
-
 int main() {
-  cout << "****************** HANGMAN ******************\n";
+  std::cout << "****************** HANGMAN ******************\n";
   // startup_animation();
   print_state(lives);
   srand(time(0));
-  string used_letters = "";
-  string computer_choice = wordlist[rand() % wordlist.size()];
+  std::string used_letters = "";
+  std::string computer_choice = wordlist[rand() % wordlist.size()];
 
   do {
     computer_choice = wordlist[rand() % wordlist.size()];
   } while (!isValid(computer_choice));
 
-  string display_string(computer_choice.size(), '_');
+  std::string display_string(computer_choice.size(), '_');
 
   for (int i = 0; i < computer_choice.size(); ++i) {
     if (rand() % 2 == 1) {
@@ -31,15 +29,15 @@ int main() {
     }
   }
   while (display_string != computer_choice) {
-    cout << "\nGuess the word: " << display_string << " ("
-         << computer_choice.size() << " characters)\n";
+    std::cout << "\nGuess the word: " << display_string << " ("
+              << computer_choice.size() << " characters)\n";
     if (used_letters.size() <= 2) {
-      cout << "Used letter: " << used_letters << "\nEnter your guess: ";
+      std::cout << "Used letter: " << used_letters << "\nEnter your guess: ";
     } else {
-      cout << "Used letters: " << used_letters << "\nEnter your guess: ";
+      std::cout << "Used letters: " << used_letters << "\nEnter your guess: ";
     }
     char guess, display_guess;
-    cin >> guess;
+    std::cin >> guess;
     if (isalpha(guess)) {
       guess = tolower(guess);
       display_guess = toupper(guess);
@@ -48,7 +46,7 @@ int main() {
       // sort(used_letters.begin(), used_letters.end()); //for the time being i
       // won't sort these neither will i do union(); to remove the repetitions
     } else {
-      cout << "--> Enter letters only <--";
+      std::cout << "--> Enter letters only <--";
     }
     bool found = false;
     for (int i = 0; i < computer_choice.size(); ++i) {
@@ -60,9 +58,10 @@ int main() {
 
     if (!found) {
       if (lives > 2) {
-        cout << "\nIncorrect guess. " << lives - 1 << " lives remaining.\n";
+        std::cout << "\nIncorrect guess. " << lives - 1
+                  << " lives remaining.\n";
       } else if (lives == 2) {
-        cout << "\nIncorrect guess. " << "1 life remaining.\n";
+        std::cout << "\nIncorrect guess. " << "1 life remaining.\n";
       }
       lives--;
       print_state(lives);
@@ -73,10 +72,11 @@ int main() {
   }
 
   if (lives == 0) {
-    cout << "\nYou lost!\nCorrect answer was '" << computer_choice << "'\n";
+    std::cout << "\nYou lost!\nCorrect answer was '" << computer_choice
+              << "'\n";
   } else {
-    cout << "\nCongratulations! You guessed the word: '" << computer_choice
-         << "'\n";
+    std::cout << "\nCongratulations! You guessed the word: '" << computer_choice
+              << "'\n";
   }
   return 0;
 } // by the genius HassanIQ777
